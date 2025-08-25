9149e4addcaeae293fc884a40143331a206cc0a5
jrodriguezneira <63802022+jrodriguezneira@users.noreply.github.com>
2025-08-25 22:55:26 +1000

    Stocks

--- src/data/dummy.js
+++ src/data/dummy.js
@@ -654,53 +654,25 @@ export const earningData = [
 ];
 
 export const recentTransactions = [
-  {
-    icon: <BsCurrencyDollar />,
-    amount: '+$350',
-    title: 'Paypal Transfer',
-    desc: 'Money Added',
-    iconColor: '#03C9D7',
-    iconBg: '#E5FAFB',
-    pcColor: 'green-600',
-  },
+
   {
     icon: <BsShield />,
     amount: '-$560',
-    desc: 'Bill Payment',
-    title: 'Wallet',
+    desc: 'products',
+    title: 'In Stock',
     iconColor: 'rgb(0, 194, 146)',
     iconBg: 'rgb(235, 250, 242)',
     pcColor: 'red-600',
   },
   {
-    icon: <FiCreditCard />,
-    amount: '+$350',
-    title: 'Credit Card',
-    desc: 'Money reversed',
-    iconColor: 'rgb(255, 244, 229)',
-    iconBg: 'rgb(254, 201, 15)',
-
-    pcColor: 'green-600',
-  },
-  {
     icon: <TiTick />,
     amount: '+$350',
-    title: 'Bank Transfer',
-    desc: 'Money Added',
+    title: 'Out of Stock',
+    desc: 'products',
 
     iconColor: 'rgb(228, 106, 118)',
     iconBg: 'rgb(255, 244, 229)',
     pcColor: 'green-600',
-  },
-  {
-    icon: <BsCurrencyDollar />,
-    amount: '-$50',
-    percentage: '+38%',
-    title: 'Refund',
-    desc: 'Payment Sent',
-    iconColor: '#03C9D7',
-    iconBg: '#E5FAFB',
-    pcColor: 'red-600',
   },
 ];
 
@@ -3163,6 +3135,9 @@ export const stackedChartData = [
     { x: 'May', y: 159.9 },
     { x: 'Jun', y: 159.9 },
     { x: 'July', y: 159.9 },
+    { x: 'Aug', y: 159.9 },
+    { x: 'Sep', y: 159.9 },
+    { x: 'Oct', y: 159.9 },
   ],
   [
     { x: 'Jan', y: 111.1 },
@@ -3185,16 +3160,6 @@ export const stackedCustomSeries = [
     background: 'blue',
 
   },
-
-  { dataSource: stackedChartData[1],
-    xName: 'x',
-    yName: 'y',
-    name: 'Expense',
-    type: 'StackingColumn',
-    background: 'red',
-
-  },
-
 ];
 
 export const stackedPrimaryXAxis = {
